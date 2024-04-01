51178af19d570bad0a85c911b20c71ab596c1e14
Vitaliy Pozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2024-04-01 20:16:22 +0300

    upd mlp autoencoder

--- fdd_defense/defenders/autoencoder.py
+++ fdd_defense/defenders/autoencoder.py
@@ -62,11 +62,13 @@ class MLPDecoder(nn.Module):
 
 
 class AutoEncoderDefender(BaseDefender, ABC):
-    def __init__(self, model, lr=0.01, training_attacker=None):
+    def __init__(self, model, lr, training_attacker, adv_coeff, num_epochs):
         super().__init__(model)
         self.training_attacker = training_attacker
         self.loss = nn.MSELoss(reduction='mean')
         self.lr = lr
+        self.adv_coeff = adv_coeff
+        self.num_epochs = num_epochs
         self.encoder = None
         self.decoder = None
     
@@ -75,17 +77,19 @@ class AutoEncoderDefender(BaseDefender, ABC):
         self.encoder.train()
         self.decoder.train()
         print('Autoencoder training...')
-        for e in trange(self.model.num_epochs, desc='Epochs ...'):
+        for e in trange(self.num_epochs, desc='Epochs ...'):
             losses = []
-            for batch, _, _ in tqdm(self.model.dataloader, desc='Steps ...', leave=False):
-                batch = torch.FloatTensor(batch).to(self.model.device)
-                target_batch = torch.FloatTensor(batch).to(self.model.device)
+            for batch, _, label in tqdm(self.model.dataloader, desc='Steps ...', leave=False):
+                batch_ = torch.FloatTensor(batch).to(self.model.device)
+                rec_batch = self.decoder(self.encoder(batch_))
+                rec_loss = self.loss(rec_batch, batch_)
+                adv_loss = 0
                 if self.training_attacker is not None:
-                    adv_batch = self.training_attacker.attack(batch)
-                    batch = torch.cat([batch, adv_batch])
-                    target_batch = torch.cat([target_batch, target_batch])
-                rec_batch = self.decoder(self.encoder(batch))
-                loss = self.loss(rec_batch, target_batch)
+                    adv_batch = self.training_attacker.attack(batch, label)
+                    batch_ = torch.FloatTensor(adv_batch).to(self.model.device)
+                    rec_batch = self.decoder(self.encoder(batch_))
+                    adv_loss = self.loss(rec_batch, batch_)
+                loss = rec_loss + self.adv_coeff * adv_loss
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
@@ -103,8 +107,16 @@ class AutoEncoderDefender(BaseDefender, ABC):
 
 
 class MLPAutoEncoderDefender(AutoEncoderDefender):
-    def __init__(self, model, lr=0.01, training_attacker=None, bottleneck_size=3):
-        super().__init__(model, lr, training_attacker)
+    def __init__(
+            self, 
+            model, 
+            lr=0.01, 
+            training_attacker=None, 
+            adv_coef=1., 
+            num_epochs=10, 
+            bottleneck_size=3
+        ):
+        super().__init__(model, lr, training_attacker, adv_coef, num_epochs)
         num_sensors = self.model.dataset.df.shape[1]
         window_size = self.model.window_size
         self.encoder = MLPEncoder(num_sensors, window_size, bottleneck_size)
--- fdd_defense/utils.py
+++ fdd_defense/utils.py
@@ -1,3 +1,7 @@
+import numpy as np
+from fddbenchmark import FDDDataloader
+from tqdm.auto import tqdm
+
 def weight_reset(model):
     """
     ref: https://discuss.pytorch.org/t/how-to-re-set-alll-parameters-in-a-network/20819/9
@@ -5,3 +9,25 @@ def weight_reset(model):
     reset_parameters = getattr(model, "reset_parameters", None)
     if callable(reset_parameters):
         model.reset_parameters()
+
+def accuracy(attacker, defender, step_size):
+    test_loader = FDDDataloader(
+        dataframe=attacker.model.dataset.df,
+        mask=attacker.model.dataset.test_mask,
+        label=attacker.model.dataset.label,
+        window_size=attacker.model.window_size,
+        step_size=step_size,
+        use_minibatches=True,
+        batch_size=512,
+    )
+    preds = []
+    labels = []
+    for sample, _, label in tqdm(test_loader):
+        pred = attacker.model.predict(sample)
+        adv_sample = attacker.attack(sample, pred)
+        pred = defender.predict(adv_sample)
+        preds.append(pred)
+        labels.append(label)
+    preds = np.concatenate(preds)
+    labels = np.concatenate(labels)
+    return (preds == labels).sum() / len(preds)
